a025d2a8a6f0e65e0af355b86f415336c6e5a0c3
David Wiens <davidmwiens@outlook.com>
2018-02-18 21:02:15 -0800

    copied files from HW07

---
+++ finalProjectServerConfig.js
@@ -0,0 +1,62 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 32180);
+
+
+// GET handler
+app.get('/',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+
+  var context = {};
+  context.dataList = qParams;
+
+  res.render('get-loopback', context);
+});
+
+// POST handler
+app.post('/', function(req,res){
+
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+
+  var bParams = [];
+  for (var p in req.body){
+    bParams.push({'name':p,'value':req.body[p]})
+  }
+
+  var context = {};
+  context.getDataList = qParams;
+  context.postDataList = bParams;
+
+  res.render('post-loopback', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
